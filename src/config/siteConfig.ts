03fae1027146c551a5299333d133ba46f5cd8a8b
Manish Kumar Das <ammanishkumardas@gmail.com>
2025-06-07 23:49:44 +0530

    footer

---
+++ src/config/siteConfig.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  companyName: 'Daily Bazzar',
+  siteUrl: 'https://dailybazzar.com',
+  Socials: {
+    xSocial: 'https://x.com/dailybazzar',
+    Github: 'https://github.com/dailybazzar',
+    Instagram: 'https://www.instagram.com/dailybazzar',
+    LinkedIn: 'https://www.linkedin.com/company/dailybazzar',
+    Email: 'admin@dailybazzar.com',
+    Phone: '+91 9876543210',
+    Location: 'JB Road, Jorhat - 785001, Assam, India',
+  }
+}; 
