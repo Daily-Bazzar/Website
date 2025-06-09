a06c553c8038eff93371e9ee8222a462a87dc051
Manish Kumar Das <ammanishkumardas@gmail.com>
2025-06-10 01:38:16 +0530

    some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,29 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [
-            tailwindcss(),
-        ]
-    }
+  vite: {
+      plugins: [
+          tailwindcss(),
+      ]
+  },
+
+  integrations: [
+    sentry({
+      dsn: "https://fcdb46b36aeb581b7aaed3c6374f7fdf@o4509470295457792.ingest.us.sentry.io/4509470297161729",
+      tracesSampleRate: 0,
+      replaysSessionSampleRate: 0,
+      replaysOnErrorSampleRate: 0,
+      // Setting this option to true will send default PII data to Sentry.
+      // For example, automatic IP address collection on events
+      sendDefaultPii: true,
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ]
 });
---
+++ src/config/links.ts
@@ -0,0 +1,2 @@
+export const joinWaitlistLink = "https://forms.gle/WtJgGuSuS3KQXJ1LA";
+export const listYourBusinessLink = "https://forms.gle/ZcSvFK2WGuXW2PUK6"
