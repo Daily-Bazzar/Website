f08fb21cb5b9b2a97ceada448a1f458353a3e77b
tohutosema77 <tohutosema77@gmail.com>
2025-09-06 12:46:19 +0530

    Initial commit

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,8 +6,15 @@ export const siteConfig = {
     // Github: 'https://github.com/dailybazzar',
     Instagram: 'https://www.instagram.com/daily_bazzar_',
     LinkedIn: 'https://www.linkedin.com/company/daily-bazzar',
+    Facebook:'https://www.facebook.com/profile.php?id=61577861526171',
     Email: 'admin@dailybazzar.com',
     Phone: '+919954059514',
     Location: 'JB Road, Jorhat - 785001, Assam, India',
+    whatsapp: 'https://www.facebook.com/profile.php?id=61577861526171',
+  },
+  applinks: {
+    playstore: 'https://www.facebook.com/profile.php?id=61577861526171',
+    apple: 'https://www.facebook.com/profile.php?id=61577861526171',
   }
+
 }; 
